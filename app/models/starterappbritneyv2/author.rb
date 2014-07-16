2d4e08cfd6d822645835e4742fece0eddbe3a675
Britney Wright <blw06g@gmail.com>
2014-07-15 20:57:41 -0400

    added path and name validation to author model

--- app/models/starterappbritneyv2/author.rb
+++ app/models/starterappbritneyv2/author.rb
@@ -1,7 +1,10 @@
 module Starterappbritneyv2
   class Author < ActiveRecord::Base
+    is_addressable path: "/authors"
     acts_as_content_block
     content_module :authors
     has_attachment :headshot
+
+    validates_presense_of :name
   end
 end
