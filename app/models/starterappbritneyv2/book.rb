1f7795c413e200bd1bea0a38c835ef3671642c63
Britney Wright <blw06g@gmail.com>
2014-07-15 20:44:06 -0400

    updated Books model for path and name validation

--- app/models/starterappbritneyv2/book.rb
+++ app/models/starterappbritneyv2/book.rb
@@ -1,7 +1,10 @@
 module Starterappbritneyv2
   class Book < ActiveRecord::Base
+    is_addressable path: "/books"
     acts_as_content_block
     content_module :books
     has_attachment :cover
+
+    validates_presence_of :name
   end
 end
